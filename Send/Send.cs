483fcf19a7f27a17b9dfa9f57c578cfa1b960b83
Ivan Komarenko <vanyakomarenko@gmail.com>
2021-01-08 09:51:36 +0200

    Add project files.

---
+++ Send/Send.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class EmitLog
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            var message = Console.ReadLine();
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "logs",
+                                 routingKey: "",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
